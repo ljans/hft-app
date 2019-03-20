0636759efb41d73f23143e20cb904f1a89492b37
Lukas Jans <ljans@luniverse.de>
2019-03-20 10:42:53 +0100

    Improve scrolling

--- src/script/client.js
+++ src/script/client.js
@@ -1,3 +1,12 @@
+// Prevent header and footer from putting the scroll focus on the body
+$('header, footer').on('touchmove', e => e.preventDefault());
+
+// Prevent the wrapper from scrolling to the limit and bubbling to the body
+$('.wrapper').on('touchstart', function(){
+	if(this.scrollTop <= 0) this.scrollTop = 1;
+	if(this.scrollTop >= this.scrollHeight - this.clientHeight) this.scrollTop = this.scrollHeight - this.clientHeight - 1;
+});
+
 // Course selector
 $('.subjects .item').map(item => {
 	
