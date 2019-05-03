2d3d2569bbb262b3a63b21a427aa864b30a3292c
Lukas Jans <ljans@luniverse.de>
2019-05-03 21:07:26 +0200

    Dissolve GUI endpoint

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@
 	
 		<title>HFT App &middot; Für dein Studium</title>
 		
-		<link rel="icon" type="image/png" href="image/favicon.png">
+		<link rel="icon" type="image/png" href="/image/favicon.png">
 		
 		<link rel="stylesheet" href="/style/intro.scss">
 		
@@ -38,21 +38,55 @@
 			</a>
 		</div>
 	
-		{{#articles}}
+		<?php foreach([[
+			'name' => 'lectures',
+			'title' => 'Dein persönlicher Stundenplan',
+			'description' => [
+				'Stelle deinen eigenen Stundenplan zusammen, indem du Kurse aus dem Vorlesungsverzeichnis auswählst.',
+				'Ausfälle oder Raumänderungen werden automatisch aus dem LSF synchronisiert.'
+			]
+		],[
+			'name' => 'meals',
+			'title' => 'Mensa-Speiseplan',
+			'description' => [
+				'Mit der App hast du immer den aktuellen Mensa-Speiseplan dabei.',
+				'Die zweiwöchige Vorschau enthält auch Bilder des Angebots, um dir die Entscheidung zu erleichtern.'
+			]
+		],[
+			'name' => 'exams',
+			'title' => 'Notenübersicht',
+			'description' => [
+				'Deine Noten werden aus dem LSF synchronisiert, sodass du alles auf einen Blick sehen kannst.',
+				'Bei neuen Prüfungsergebnissen erhältst du automatisch eine Benachrichtigung.'
+			]
+		],[
+			'name' => 'events',
+			'title' => 'Hochschul-Terminkalender',
+			'description' => [
+				'Verpasse keine wichtigen Hochschul-Termine mehr mit dem eingebauten Kalender.',
+				'Die aktuellen Termine sind für dich auch auf der Startseite zusammengefasst.'
+			]
+		],[
+			'name' => 'menu',
+			'title' => 'Weitere Inhalte',
+			'description' => [
+				'Darüber hinaus enthält die App viele weitere nützliche Funktionen rund um das Hochschulleben.',
+				'Hier kannst du dich auch abmelden, um die Verknüpfung mit deinem Gerät wieder aufzuheben.'
+			]
+		]] as $article) print '
 			<article>
 				<div class="container">
 					<div class="description">
-						<h2>{{title}}</h2>
-						{{#description}}
-							<p>{{.}}</p>
-						{{/description}}
+						<h2>'.$article['title'].'</h2>
+						<p>'.$article['description'][0].'</p>
+						<p>'.$article['description'][1].'</p>
 					</div>
 					<div class="screenshot">
-						<img src="/image/intro/{{name}}.png" alt="{{name}}">
+						<img src="/image/intro/'.$article['name'].'.png">
 					</div>
 				</div>
 			</article>
-		{{/articles}}
+		'; ?>
 	
 		<div class="banner">
 			<div class="container">
