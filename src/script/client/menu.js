95dca236f5dcc23509a3d738ca88358aeb50f048
Lukas Jans <ljans@luniverse.de>
2019-05-04 17:06:39 +0200

    Remove A2HS link

--- src/script/client/menu.js
+++ src/script/client/menu.js
@@ -1,19 +1,3 @@
-// Select installer elements
-const $installer = $('.installer');
-
-// Show installer if not launched standalone
-if(!window.matchMedia('(display-mode: standalone)').matches && !window.navigator.standalone) $installer.toggleClass('hidden', false);
-
-// Catch prompt
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', e => deferredPrompt = e);
-
-// Register click handler
-$installer.on('click', () => {
-	if(deferredPrompt) deferredPrompt.prompt();
-	else alert('Öffne die Seitenoptionen deines Browsers und wähle "Zum Homebildschirm".');
-});
-
 // Logout confirmation
 $('.logout').on('click', e => {
 	if(!confirm('Möchtest du dich wirklich abmelden?\nDadurch werden alle deine Daten auf diesem Gerät gelöscht.')) e.preventDefault();
